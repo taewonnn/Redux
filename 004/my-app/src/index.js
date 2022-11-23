b55eeee7de1efafc6d8ab34affbccfc644e9c378
TaeWon Park <thisis_tw94@naver.com>
2022-11-23 15:54:40 +0900

    lecture: redux

--- 004/my-app/src/index.js
+++ 004/my-app/src/index.js
@@ -22,4 +22,3 @@ root.render(
         <App />
     </Provider>
 );
-Footer
--- 004/my-app/src/modules/stockcounter.jsx
+++ 004/my-app/src/modules/stockcounter.jsx
@@ -1,33 +1,33 @@
 // 액션 생성함수
 export const sale = () => {
-  return { type: "SALE" };
+    return { type: "SALE" };
 };
 
 export const soldout = () => {
-  return { type: "SOLD_OUT" };
+    return { type: "SOLD_OUT" };
 };
 
 // 초기 값
 const initialState = {
-  message: '판매중!!',
+    message: '판매중!!',
 };
 
 // 리듀서
 const stockReducer = (state = initialState, action) => {
-  switch (action.type) {
-      case "SALE":
-          return {
-              ...state,
-              message : '판매중!!'
-          }
-      case "SOLD_OUT":
-          return {
-              ...state,
-              message : '완판!!'
-          }
-      default:
-          return state;
-  }
+    switch (action.type) {
+        case "SALE":
+            return {
+                ...state,
+                message : '판매중!!'
+            }
+        case "SOLD_OUT":
+            return {
+                ...state,
+                message : '완판!!'
+            }
+        default:
+            return state;
+    }
 };
 
 export default stockReducer;
